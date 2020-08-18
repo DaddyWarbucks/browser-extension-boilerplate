a5c9a2eb22c77efa5671764d5bf7d8617d05a755
DaddyWarbucks <beau@pxlpotion.com>
2020-08-18 04:18:53 -0500

    Basic options page

--- src/options.js
+++ src/options.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const OptionsPage = () => {
-  const handleClick = event => console.log(event);
+  const handleClick = (event) => console.log(event);
   return (
-    <div style={{ width: 300 }}>
-      <ListGroup>
-        <ListGroupItem tag="button" action onClick={handleClick}>
-          <p className="mb-1 font-weight-bold lead">Do a thing</p>
-          <p className="mb-0">
-            Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
-            eget risus varius blandit.
-          </p>
-        </ListGroupItem>
-      </ListGroup>
-    </div>
+    <ListGroup>
+      <ListGroupItem tag="button" action onClick={handleClick}>
+        <p className="mb-1 font-weight-bold lead">Do a thing</p>
+        <p className="mb-0">
+          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
+          eget risus varius blandit.
+        </p>
+      </ListGroupItem>
+    </ListGroup>
   );
 };
 
